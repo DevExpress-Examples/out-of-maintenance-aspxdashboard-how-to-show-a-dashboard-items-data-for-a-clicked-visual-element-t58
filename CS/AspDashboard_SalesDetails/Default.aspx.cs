38f2c73cc386a3e5f5d11df2ab38763654d5e3d0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/AspDashboard_SalesDetails/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using DevExpress.DashboardWeb;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AspDashboard_SalesDetails {
+    public partial class Default : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            DashboardFileStorage dashboardStorage = new DashboardFileStorage(@"~/App_Data/Dashboards");
+            ASPxDashboard1.SetDashboardStorage(dashboardStorage);
+        }
+
+        protected void OnDataLoading(object sender, DataLoadingWebEventArgs e) {
+            SalesDetailsDataGenerator salesDetailsDataGenerator = new SalesDetailsDataGenerator(LoadData("DashboardSales"));
+            salesDetailsDataGenerator.Generate();
+            e.Data = salesDetailsDataGenerator.Data;
+        }
+
+        static string GetRelativePath(string name) {
+            return Path.Combine(HostingEnvironment.MapPath("~"), "App_Data", "Data", name);
+        }
+        static DataSet LoadData(string fileName) {
+            string path = GetRelativePath(string.Format("{0}.xml", fileName));
+            DataSet ds = new DataSet();
+            ds.ReadXml(path, XmlReadMode.ReadSchema);
+            return ds;
+        }
+
+        protected void ASPxWebDashboard1_ConfigureItemDataCalculation(object sender, ConfigureItemDataCalculationWebEventArgs e) {
+            e.CalculateAllTotals = true;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# ASPxDashboard - How to show a dashboard item's data for a clicked visual element
+
+
+<p>This example demonstrates how to obtain <a href="https://documentation.devexpress.com/Dashboard/18078/Building-the-Designer-and-Viewer-Applications/Web-Dashboard/Obtaining-Underlying-and-Displayed-Data">client and underlying data</a> corresponding to a particular visual element and display this data in a popup. You can click individual cards within the <a href="https://documentation.devexpress.com/Dashboard/117163/Creating-Dashboards/Creating-Dashboards-on-the-Web/Designing-Dashboard-Items/Cards">Card</a> dashboard item to see detailed information.</p>
+
+<br/>
+
+
